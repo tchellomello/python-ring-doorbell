ab5f3061de4b1fc58048f5d900e549595b4ae702
balloobbot <balloobbot@users.noreply.github.com>
2026-08-22 22:54:09 +0200

    Stop using deprecated aiohttp.BasicAuth
    aiohttp.BasicAuth and the request `auth` parameter are deprecated and
    will be removed in aiohttp 4.0.

--- ring_doorbell/auth.py
+++ ring_doorbell/auth.py
@@ -5,11 +5,12 @@ from __future__ import annotations
 
 import uuid
 from asyncio import TimeoutError
+from base64 import b64encode
 from functools import cached_property
 from json import loads as json_loads
 from typing import TYPE_CHECKING, Any, Callable, ClassVar
 
-from aiohttp import BasicAuth, ClientError, ClientResponseError, ClientSession
+from aiohttp import ClientError, ClientResponseError, ClientSession
 from oauthlib.common import urldecode
 from oauthlib.oauth2 import (
     LegacyApplicationClient,
@@ -26,6 +27,16 @@ from ring_doorbell.exceptions import (
     RingTimeout,
 )
 from ring_doorbell.util import _DeprecatedSyncApiHandler
+
+
+def _basic_auth_header(login: str, password: str) -> str:
+    """Return the value of an HTTP Basic ``Authorization`` header.
+
+    ``aiohttp.BasicAuth`` is deprecated and goes away in aiohttp 4.0. Its
+    replacement, ``aiohttp.encode_basic_auth()``, needs aiohttp 3.14, which
+    aioresponses does not support yet.
+    """
+    return "Basic " + b64encode(f"{login}:{password}".encode()).decode()
 
 
 class Auth:
@@ -65,7 +76,7 @@ class Auth:
         self._oauth_client = LegacyApplicationClient(
             client_id=OAuth.CLIENT_ID, token=token
         )
-        self._auth = BasicAuth(OAuth.CLIENT_ID, "")
+        self._auth_header = _basic_auth_header(OAuth.CLIENT_ID, "")
 
     @property
     def _session(self) -> ClientSession:
@@ -84,7 +95,11 @@ class Auth:
         :type password: str
         :type otp_code: str.
         """
-        headers = {"User-Agent": self.user_agent, "hardware_id": self.hardware_id}
+        headers = {
+            "User-Agent": self.user_agent,
+            "hardware_id": self.hardware_id,
+            "Authorization": self._auth_header,
+        }
 
         if otp_code:
             headers["2fa-support"] = "true"
@@ -100,7 +115,6 @@ class Auth:
                 OAuth.ENDPOINT,
                 data=data,
                 headers=headers,
-                auth=self._auth,
             )
             async with resp:
                 text = await resp.text()
@@ -123,13 +137,14 @@ class Auth:
             headers = {
                 "Accept": "application/json",
                 "Content-Type": ("application/x-www-form-urlencoded;charset=UTF-8"),
+                "Authorization": self._auth_header,
             }
             body = self._oauth_client.prepare_refresh_body(
                 refresh_token=self._token["refresh_token"]
             )
             data = dict(urldecode(body))
             resp = await self._session.request(
-                "POST", OAuth.ENDPOINT, data=data, headers=headers, auth=self._auth
+                "POST", OAuth.ENDPOINT, data=data, headers=headers
             )
             async with resp:
                 text = await resp.text()
